6a1972d80cf3c16dd8ffbd23ce6911673c239f06
Andrey Uvarov <uvarov6799@gmail.com>
2023-09-21 20:34:22 +0300

    подключен hook-form

--- src/components/MainBlock.tsx
+++ src/components/MainBlock.tsx
@@ -2,19 +2,24 @@ import React from 'react'
 import Task from './Task'
 import TaskPopup from './TaskPopup'
 
+
+
 export default function MainBlock() {
 
-	// const addTask = 
+	const [popUp, setPopUp] = React.useState(false);
 
+	const onTask = () => {
+		setPopUp(true);
+	}
 	return (
+
 		<div className='mainBlock'>
 			<div className="block-title">
 				<div className="name">Block name</div>
-				<button className="add-task">add new task</button>
+				<button onClick={onTask} className="add-task">add new task</button>
 			</div>
 			<Task />
-			<Task />
-			<TaskPopup />
+			{popUp ? <TaskPopup /> : ''}
 		</div>
 	)
 }
--- src/components/TaskPopup/index.tsx
+++ src/components/TaskPopup/index.tsx
@@ -1,15 +1,48 @@
-import React from 'react'
-import styles from '../Task/Task.module.scss';
+import React, { useId } from 'react'
+import styles from '../Task/Task.module.scss'
+import { SubmitHandler, useForm } from 'react-hook-form';
 
-export default function TaskPopup() {
+type TaskType = {
+	title: string | void;
+	text: string | void
+	label: string | void
+}
+const TaskPopup = () => {
+
+
+	const id = useId()
+	const { register, handleSubmit, formState: { errors } } = useForm<TaskType>()
+	const onSubmit: SubmitHandler<TaskType> = (data) => {
+		console.log(data)
+	}
+
+
 	return (
-		<form className={styles.pop__task}>
+		<form onSubmit={handleSubmit(onSubmit)} className={styles.pop__task}>
 			<div className={styles.input__block}>
-				<input className={styles.pop__title} placeholder='task title' />
-				<input type='text' className={styles.pop__text} placeholder='task text' />
-				<input className={styles.pop__label} placeholder='task label' />
+				<label htmlFor={`${id}-'title'`} />
+				<input id={`${id}-'title'`} type='title' className={styles.pop__title} placeholder='task title'
+					{...register('title', { required: 'Title is required' })}
+				/>
+				{errors?.title && <div style={{ color: 'red' }}>{errors.title.message}</div>}
+
+				<label htmlFor={`${id}-'text'`}></label>
+				<input id={`${id}-'text'`} type='text' className={styles.pop__text} placeholder='task text'
+					{...register('text')}
+				/>
+
+				{/* Форму удобнее привязать через htmlFor  */}
+				<label className={styles.label__container}>
+					Укажите теги:
+					<input className={styles.pop__label} type='label' placeholder='task label'
+						{...register('label')}
+					/>
+				</label>
+
 			</div>
 			<input type='submit' value={'add task'} className={styles.pop__button} />
 		</form>
 	)
 }
+
+export default TaskPopup
